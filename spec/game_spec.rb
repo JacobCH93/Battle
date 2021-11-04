6266b1572042c310eb4010b5fda9deab9cab174a
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-04 16:12:53 +0000

    unfinished skinny_controllers challenge

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,33 @@
 require 'game'
-describe Game do
-    describe '#attack' do
-    subject(:game) { Game.new }
-    let(:player_1) { double :player }
-    let(:player_2) { double :player }
 
-    it 'Allows the player to receive damage' do
-        expect(player_2).to receive(:receive_damage)
-        game.attack(player_2)
+describe Game do
+
+    describe '#attack' do
+
+      subject(:game) { Game.new(player_1, player_2) }
+      let(:player_1) { double :player }
+      let(:player_2) { double :player }
+
+
+      it 'Allows the player to receive damage' do
+          expect(player_2).to receive(:receive_damage)
+          game.attack(player_2)
+      end
+
     end
-end
+
+    describe '#player_1' do
+      it 'retrieves the first player' do
+        expect(game.player_1).to eq player_1
+      end
+    end
+  
+    describe '#player_2' do
+      it 'retrieves the second player' do
+        expect(game.player_2).to eq player_2
+      end
+    end
+
+
+
 end
